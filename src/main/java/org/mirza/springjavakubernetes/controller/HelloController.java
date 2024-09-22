39a8ac67810381241828a0be28191de5a8ef68ba
mirz <mirza.romi@doku.com>
2024-09-22 21:23:19 +0700

    add log

--- src/main/java/org/mirza/springjavakubernetes/controller/HelloController.java
+++ src/main/java/org/mirza/springjavakubernetes/controller/HelloController.java
@@ -1,13 +1,16 @@
 package org.mirza.springjavakubernetes.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     @GetMapping("/")
     public ResponseEntity helloController() {
+        log.info("Hello Controller hit!");
         return ResponseEntity.ok("Hi, this is v1 project");
     }
 }
