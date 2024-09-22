4b0f491e4fc036244ad9adc6f175fb59a0327773
mirz <mirza.romi@doku.com>
2024-09-22 21:12:09 +0700

    first commit

---
+++ src/main/java/org/mirza/springjavakubernetes/SpringJavaKubernetesApplication.java
@@ -0,0 +1,13 @@
+package org.mirza.springjavakubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mirza/springjavakubernetes/SpringJavaKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mirza.springjavakubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
